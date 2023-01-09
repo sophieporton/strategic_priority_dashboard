e9a9a7d845a0d4b796014df891079890e1a3278d
st-augustine <sophie.porton@towerhamlets.ac.uk>
2023-01-09 12:24:24 +0000

    fixed hover text

--- pages/Priority 1- Tackling the cost of living crisis.py
+++ pages/Priority 1- Tackling the cost of living crisis.py
@@ -20,7 +20,7 @@ def plot_wards(df, column='', string='', agg='',title=''):
                    hover_name=df.dissolve(by='ward_name',aggfunc ={'OBS_VALUE':agg}).index )
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(coloraxis_colorbar=dict(title=title))
-    fig.update_traces(hovertemplate='Ward name: <b>%{location}<b/> <br>Percentage: <b>%{z:.2f}%<b/>')
+    fig.update_traces(hovertemplate='Ward name: <b>%{location}</b> <br>Percentage: <b>%{z:.2f}%</b>')
     st.plotly_chart(fig,use_container_width = True)
 
 # %%
