b30c5bc134ce16da712f3efeaf32aa32d1ef2a50
st-augustine <sophie.porton@towerhamlets.ac.uk>
2023-01-17 14:51:33 +0000

    2011 percent fix

--- pages/Priority 1- Tackling the cost of living crisis.py
+++ pages/Priority 1- Tackling the cost of living crisis.py
@@ -56,29 +56,36 @@ deprivation_merge=merge_spatial_data(merged_wd_oa, deprivation_oa,"OA21CD", "GEO
 try:
   deprivation_oa_2011 = pd.read_csv('lbth_census_2011_deprivation_oa.csv')
 except:
-  deprivation_oa_2011 = pd.read_csv('https://www.nomisweb.co.uk/api/v01/dataset/NM_519_1.data.csv?date=latest&geography=1254117467...1254118025,1254259398...1254259586&rural_urban=0&c_deprived=1...5&measures=20100')
-
+  deprivation_oa_2011 = pd.read_csv('https://www.nomisweb.co.uk/api/v01/dataset/NM_519_1.data.csv?date=latest&geography=1254117467...1254118025,1254259398...1254259586&rural_urban=0&c_deprived=0...5&measures=20100')
+  deprivation_oa_2011.to_csv('lbth_census_2011_deprivation_oa.csv')
+  
 #merge deprivation data with spatial data
 deprivation_merge_2011=merge_spatial_data(merged_wd_oa, deprivation_oa_2011,"OA21CD", "GEOGRAPHY_CODE")
 
-deprivation_merge_2011['OBS_VALUE'] = (
-    np.select(
-        condlist=[deprivation_merge_2011['GEOGRAPHY_NAME'] == 'Tower Hamlets', deprivation_merge_2011['GEOGRAPHY_NAME'] =='England', deprivation_merge_2011['GEOGRAPHY_NAME'] =='London'],
-        choicelist=[(deprivation_merge_2011['OBS_VALUE']/101257)*100,(deprivation_merge_2011['OBS_VALUE']/22063368)*100,(deprivation_merge_2011['OBS_VALUE']/3266173)*100] 
-        ))
-
 # %%
-#rename column deprivation column to allow for concatenate
+#rename column 
 
 deprivation_merge_2011.rename(columns={'C_DEPRIVED_NAME': 'C2021_DEP_6_NAME'}, inplace=True)
 
 # %%
 # Replace multiple substrings
 
-deprivation_2011= deprivation_merge_2011.apply(lambda x: x.replace({'one':'1','two': '2',
+deprivation_merge_2011= deprivation_merge_2011.apply(lambda x: x.replace({'one':'1','two': '2',
     'three': '3','four': '4'},regex=True))
 
+# %%
+#oa_total_list= deprivation_oa_2011['C_DEPRIVED_NAME']=='All categories: Classification of household deprivation'
 
+oa_total_list= deprivation_merge_2011.loc[deprivation_merge_2011['C2021_DEP_6_NAME']=='All categories: Classification of household deprivation','OBS_VALUE'].tolist()
+
+
+# %%
+#create new column in df
+
+deprivation_merge_2011['totals']= np.repeat(oa_total_list,6)
+
+# %%
+deprivation_merge_2011['OBS_VALUE']= (deprivation_merge_2011['OBS_VALUE']/ deprivation_merge_2011['totals'])*100
 
 # %%
 #read in relative deprivation 2021 dataset
